41e5cb7a904327ba875924ca9799aa5fbfa14b1f
yZemp <maurizio.3rrico@gmail.com>
2024-10-02 10:22:21 +0200

    test

--- lec1/ex1.c
+++ lec1/ex1.c
@@ -1,7 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main() {
-    printf("Hello World!\n");
+int main(int argc, char *argv[]) {
+    float f = 1.2e34;
+    for (size_t i = 0; i < 24; i++) {
+        f *= 2;
+        printf("%d:\t%d", i, f);
+    }
+    
+
     return(0);
 }

4808817b7ded5de18d9285a7dc28552e87da76f3
yZemp <maurizio.3rrico@gmail.com>
2024-10-02 10:24:27 +0200

    test

--- lec1/ex1.c
+++ lec1/ex1.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]) {
     float f = 1.2e34;
     for (size_t i = 0; i < 24; i++) {
         f *= 2;
-        printf("%d:\t%d", i, f);
+        printf("%d:\t%d\n", i, f);
     }
     
 

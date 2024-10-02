3828135db1096685c2cb598fbca65bdef28ccccd
yZemp <maurizio.3rrico@gmail.com>
2024-10-02 10:32:41 +0200

    test

--- lec1/ex1.c
+++ lec1/ex1.c
@@ -8,13 +8,13 @@ int main(int argc, char *argv[]) {
     float f1 = 1.2e34;
     for (size_t i = 0; i < 24; i++) {
         f1 *= 2;
-        printf("%d:\t%f\n", i, f1);
+        printf("%d:\t%.20e\n", i, f1);
     }
     printf("Double:\n");
     double d1 = 1.2e34;
     for (size_t i = 0; i < 24; i++) {
         d1 *= 2;
-        printf("%d:\t%f\n", i, d1);
+        printf("%d:\t%.20e\n", i, d1);
     }
     
     printf("Second cycle\n");
@@ -23,14 +23,14 @@ int main(int argc, char *argv[]) {
     float f2 = 1.2e304;
     for (size_t i = 0; i < 24; i++) {
         f2 /= 2;
-        printf("%d:\t%f\t%f\n", i, f2, 1 + f2);
+        printf("%d:\t%.20e\t%.20e\n", i, f2, 1 + f2);
     }
 
     printf("Double:\n");
     double d2 = 1.2e304;
     for (size_t i = 0; i < 24; i++) {
         d2 /= 2;
-        printf("%d:\t%f\t%f\n", i, d2, 1 + d2);
+        printf("%d:\t%.20e\t%.20e\n", i, d2, 1 + d2);
     }
 
     return(0);

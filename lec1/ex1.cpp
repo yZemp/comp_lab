05d30a72fa9182cd2cc90c837950b48c66c6602d
yZemp <maurizio.3rrico@gmail.com>
2024-10-02 11:33:32 +0200

    std adj

---
+++ lec1/ex1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+double exp_series(double x) {
+    
+}
+
+int main(int argc, char *argv[]) {
+    cout << "Hello World!";
+
+    return(0);
+}

081d3c3eedb27946f3b3cada8e0acea6484463ab
yZemp <maurizio.3rrico@gmail.com>
2024-10-02 10:29:05 +0200

    test

--- lec1/ex1.c
+++ lec1/ex1.c
@@ -2,12 +2,36 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+    printf("First cycle\n");
+    
+    printf("Single:\n");
     float f = 1.2e34;
     for (size_t i = 0; i < 24; i++) {
         f *= 2;
-        printf("%d:\t%d\n", i, f);
+        printf("%d:\t%f\n", i, f);
+    }
+    printf("Double:\n");
+    double d = 1.2e34;
+    for (size_t i = 0; i < 24; i++) {
+        d *= 2;
+        printf("%d:\t%f\n", i, d);
     }
     
+    printf("Second cycle\n");
+
+    printf("Signle:\n");
+    float f = 1.2e304;
+    for (size_t i = 0; i < 24; i++) {
+        f /= 2;
+        printf("%d:\t%f\t%f\n", i, f, 1 + f);
+    }
+
+    printf("Double:\n");
+    double d = 1.2e304;
+    for (size_t i = 0; i < 24; i++) {
+        d /= 2;
+        printf("%d:\t%f\t%f\n", i, d, 1 + d);
+    }
 
     return(0);
 }

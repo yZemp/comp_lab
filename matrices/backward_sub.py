02ef4dbff9fbe34cbaf00f9e82842b8425fb4077
yZemp <maurizio.3rrico@gmail.com>
2024-10-03 12:20:01 +0200

    std adj

---
+++ matrices/backward_sub.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+# Solve system: Ux = b
+# mat = U
+
+
+def backward_sub(mat, b):
+    if not len(mat) == len(mat[0]) == len(b): return -1
+
+    
+
+    return np.array([1, 2, 3])
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    mat = np.array([[2, 1, 1], [0, 1, -2], [0, 0, 1]])
+    b = np.array([1, -1, 4])
+    
+    print(mat)
+
+

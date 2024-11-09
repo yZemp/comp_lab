f5df9dcec0d52a48e8ebe280163e80b8db9e0ebb
yZemp <maurizio.3rrico@gmail.com>
2024-11-09 19:02:35 +0100

    Moved a file and experimented with splines

--- interpolation/nth_order_splines.py
+++ interpolation/nth_order_splines.py
@@ -18,7 +18,7 @@ INTERVAL = (-1, 1)
 colors = [(.5, .1, .8), (.5, .8, .1), (.8, .1, .5)]
 
 
-def spline(x, f):
+def linear_spline(x, f, order):
     '''
     Perform a piecewise interpolation given points with coordinate x, f
 
@@ -42,20 +42,67 @@ def spline(x, f):
     return interp
 
 
-def linear_spline(sample, delta, color_index = 1):
+def quadratic_spline(x, f, order):
     '''
-    Perform piecewise interpolation of the runge function, given a specific sample
+    Perform a piecewise interpolation given points with coordinate x, f
+
+    We need to find a_i, b_i for every interval, so that
+        f_i = a_i + b_i * x_i
     '''
 
+    # Creating block matrix (row by row)
+    mat = np.zeros((len(x) * (order + 1), len(x) * (order + 1)))
+    # print(len(mat[0]))
+    
+    for k in range(0, len(mat), 3):
+        
+        # Creating blocks of three
+        block = mat[k:k + 3]
+
+        # Iterating the block
+        for i, row in enumerate(block):
+            # if i >= 
+            # if i < 2:
+            for j in range(order + 1):
+                print(k, i, j)
+                row[k + j] = 1
+                # tmp = np.power(x[k // 3 + i], j)
+            # else:
+                # pass
+                # for j in range((order) * 2):
+                #     row[k + j] = np.power(x[k + j], j)
+                #     print(k, i, j)
+
+
+    # Creating known array of f_i
+    f_system = np.array([[f[i], f[i + 1], 0] for i in range(len(f))]).flatten()[1:-1]
+    # print(f_system)
+
+    print(mat, f_system)
+
+    # Finding array of a_i, b_i
+    interp, _, _ = solve_linear_system(mat, f_system)
+    # print(interp)
+
+    return interp
+
+
+def main(sample, delta, spline_and_order = [linear_spline, 1], color_index = 1):
+    '''
+    Perform piecewise interpolation of the runge function, given a specific sample
+    with a polynomial of arbitrary order
+    '''
+
+    spline, order = spline_and_order
+
     f = [runge(x) for x in sample]
-    interp = spline(sample, f)
+    interp = spline(sample, f, order)
     # Creating polynomial for every interval
-    polynomials = [Polynomial(interp[i:i + 2]) for i in range(0, len(interp) - 2, 2)]
+    polynomials = [Polynomial(interp[i:i + order + 1]) for i in range(0, len(interp + 1) - (order + 1), order + 1)]
     # print(len(polynomials))
 
     arrx = np.linspace(*INTERVAL, num = 1_000)
     data = [polynomials[min(max(next(i for i, xbar in enumerate(sample) if xbar >= x) - 1, 0), delta - 3)].evaluate(x) for x in arrx]
-    # data = [max(next(i for i, xbar in enumerate(sample) if xbar >= x) - 1, 0) for x in arrx]
     # print(data)
     # exit()
 
@@ -73,7 +120,8 @@ if __name__ == "__main__":
     # TODO: Unsure how to generate the interval
     # intervals = np.linspace(*INTERVAL, num = int((INTERVAL[1] - INTERVAL[0]) / DELTA))
 
-    # Uniform sample
+    # Collection of all order of splines
+    splines = [[linear_spline, 1], [quadratic_spline, 2]]
 
     fig, ax = plt.subplots(1)
     # fig.suptitle("Uniform sampling")
@@ -81,9 +129,15 @@ if __name__ == "__main__":
     fig.set_size_inches(20, 10, forward = True)
 
     uniform = np.linspace(*INTERVAL, num = 11)
-    sample, arrx, data, f = linear_spline(uniform, 11, color_index = 0)
+    sample, arrx, data, f = main(uniform, 11, splines[0], color_index = 0)
     uniform = np.linspace(*INTERVAL, num = 12)
-    sample, arrx, data, f = linear_spline(uniform, 12, color_index = 1)
+    sample, arrx, data, f = main(uniform, 12, splines[0], color_index = 1)
+
+    uniform = np.linspace(*INTERVAL, num = 2)
+    print(uniform)
+    sample, arrx, data, f = main(uniform, 2, splines[1], color_index = 0)
+    # uniform = np.linspace(*INTERVAL, num = 12)
+    # sample, arrx, data, f = main(uniform, 12, splines[0], color_index = 1)
 
     #TODO: Quadratic spline
 
